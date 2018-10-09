ab5a68675e5d7b7adb0b1d901e01a1b764ce9f1c
DELL <sonipaty98@hotmail.es>
2018-10-09 15:58:19 -0500

    Cambio 4

---
+++ 01Javascript/01-Variables.js
@@ -0,0 +1,35 @@
+var nombreVariable = 'valorVariable';
+var edad = 1;//number
+var edad3 = 1.1;//number
+var edad2 = '1';//string
+var casado = false;//boolean
+var amigable = null;//??
+var existeOno;
+var fechaNacimiento = new Date('1989-06-10');
+console.log(typeof edad);
+console.log(typeof edad2);
+console.log(typeof edad3);
+console.log(typeof casado);
+console.log(typeof amigable);
+console.log(typeof existeOno);
+console.log('la fecha de nacimiento', typeof  fechaNacimiento);
+
+//tyof devuelve el tio de dato
+
+
+// javscript es un lenguaje no tipado
+// tipado tiene tipos,, no tipado no tiene tipos
+//var edad = 10;
+
+//Javascript
+var adrian = {
+    "nombre": 'Adrian',
+    edad: 29,
+    hijos: undefined,
+    llave: "valor",
+};
+console.log(adrian );
+var adrianJSON={
+    "llave":"valor"
+};
+
