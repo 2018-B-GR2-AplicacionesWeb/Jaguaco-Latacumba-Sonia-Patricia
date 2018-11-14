bdf0c3460b3a7bde8c4c1cb4c6e5d114685ca41e
DELL <sonipaty98@hotmail.es>
2018-11-13 19:14:46 -0500

    nest

---
+++ 02-typescripte/01-tipos-variables.js
@@ -0,0 +1,32 @@
+//
+const nombre = "Adrian";
+const edad = 21.2;
+const casado = false;
+const adrian = {
+    nombre: "Adrian",
+    edad1: 2,
+    cassdo: false,
+    fecha: new Date(),
+    saludar: () => {
+        return "";
+    }
+};
+//adrian.estado(activo?);
+const arregloNumero = [1, 2, 3, 4];
+arregloNumero.push(3);
+arregloNumero.push("kk");
+let edad1 = 21., tsc;
+01 - tipos - variables.ts--;
+target;
+es2017;
+3;
+function saludar(nombre, aellido, ...otrasfunciones) {
+    return "hola";
+}
+//let sal=<number>saludar("s",aellido: "dd",otrasfunciones(1,2,3));
+//const saludardos{};
+class Usuario {
+}
+const usuario = {
+    nombre: "Adrian"
+};
