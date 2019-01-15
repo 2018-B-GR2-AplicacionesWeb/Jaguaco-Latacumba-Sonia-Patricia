911e6889571fde4dca958a6d2c65dce15d054cfd
DELL <sonipaty98@hotmail.es>
2019-01-15 16:00:26 -0500

    cambio11

--- 03-http/videos/src/main.ts
+++ 03-http/videos/src/main.ts
@@ -3,6 +3,8 @@ import {NestFactory} from '@nestjs/core'; // ts
 // import * as httpserver from 'http-server'; // js
 import {Options} from 'http-server'; // js
 import {AppModule} from './app.module';
+import * as express from 'express';
+import * as session from 'express-session';
 //import {} from './mi-codigo';
 //const a = require('./mi-codigo').a;
 
@@ -11,6 +13,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.set('view engine', 'ejs');
+    app.use(express.static('publico'));
 
     await app.listen(3000);
 }
